1aba04bdd41096d863ff8268a6a5305a45ccb817
Nikola-Obradovic <nikolao2003@outlook.com>
2026-02-11 14:36:03 +0100

    fix for deploy

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,5 @@
 import { useNavigate, useLocation } from 'react-router-dom';
-import { Mail, Phone, MapPin, Linkedin, Twitter } from 'lucide-react';
+import { Mail, Phone, MapPin} from 'lucide-react';
 import logo from '../../assets/logo.svg';
 
 const footerLinks = {
